6bdf9e72b61c5049f119658a545379133c26d083
Joy Sóuza <joysouza.contato@gmail.com>
2024-09-10 10:32:44 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ConhecaAC3 from './components/ConhecaAC3';
+import FaixaAnimada from './components/faixaAnimada';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <ConhecaAC3 />
+      </div>
+      <FaixaAnimada />
+    </>
+  );
+}
+
+export default App
---
+++ src/components/conhecaAC3.jsx
@@ -0,0 +1,19 @@
+import './conhecaAC3.css';
+
+function ConhecaAC3() {
+    return (
+        <section className='conheca-ac3'>
+            <div className='conheca-ac3-text'>
+                <h1>Por que escolher a <span className='color-red'>Código Certo Coders?</span></h1>
+                <p>Na Código Certo Coders, acreditamos que o desenvolvimento pessoal e profissional na área de tecnologia vai além do aprendizado técnico, ele envolve colaboração, inovação e a construção de redes de contatos valiosas.</p>
+                <p>Oferecemos uma ampla gama de recursos e oportunidades para que você possa expandir suas habilidades, participar de projetos práticos e conectar-se com mentores experientes que irão guiar o seu crescimento, nossa comunidade é um ambiente vibrante e acolhedor, onde a troca de conhecimento e a cooperação são incentivadas.</p>
+                <button>Entrar em contato</button>
+            </div>
+            <div className='conheca-ac3-img'>
+                <img src='src/assets/images/avatar_c3.webp' alt='Código Certo Coders'></img>
+            </div>
+        </section>
+    )
+}
+
+export default ConhecaAC3;
